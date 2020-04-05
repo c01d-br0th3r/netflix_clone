95d1f5822824b76c226e7a78fb8f594046a0c708
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-04-06 01:42:31 +0900

    make Header

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vh;
+  height: 70px;
+  display: flex;
+  padding: 10px;
+  * {
+    margin: 15px;
+  }
+  align-items: center;
+`;
+const Logo = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  margin-right: 60px;
+  color: #e74c3c;
+`;
+const Menu = styled.div`
+  font-size: 14px;
+  font-weight: 400;
+  margin-right: 30px;
+`;
+
+const Header = () => {
+  return (
+    <Container>
+      <Logo>ChanFlix</Logo>
+      <Menu>Movies</Menu>
+      <Menu>TV Shows</Menu>
+      <Menu>Search</Menu>
+    </Container>
+  );
+};
+
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import PopularMovies from "./popularMovies";
 import MainPoster from "./MainPoster";
 import GlobalStyle from "./GlobalStyles";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div>
       <GlobalStyle />
+      <Header />
       <MainPoster />
       <PopularMovies />
     </div>
