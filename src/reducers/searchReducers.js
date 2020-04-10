d57991a0dab4b5e69e6c87d165a29136db00b9b0
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-04-10 18:43:51 +0900

    edit SearchReducers, dont need term type

--- src/reducers/searchReducers.js
+++ src/reducers/searchReducers.js
@@ -1,7 +1,5 @@
 const search = (state = { loading: true }, action) => {
   switch (action.type) {
-    case "SEARCH_MOVIES":
-      return { ...state, term: action.term };
     case "SUCCESS_SEARCH_MOVIES":
       return { ...state, movies: action.movies, loading: false };
     case "FAIL_SEARCH_MOVIES":
--- src/reducers/searchTVReducers.js
+++ src/reducers/searchTVReducers.js
@@ -1,7 +1,5 @@
 const searchTV = (state = { loading: true }, action) => {
   switch (action.type) {
-    case "SEARCH_TV":
-      return { ...state, term: action.term };
     case "SUCCESS_SEARCH_TV":
       return { ...state, tvs: action.tvs, loading: false };
     case "FAIL_SEARCH_TV":
