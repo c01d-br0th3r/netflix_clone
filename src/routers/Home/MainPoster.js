7bd1492dcbd0ca4adea1faaa2cece5f28a513672
c01d-br0th3r <60615552+c01d-br0th3r@users.noreply.github.com>
2020-04-12 03:00:26 +0900

    change MainPoster

--- src/routers/Home/MainPoster.js
+++ src/routers/Home/MainPoster.js
@@ -8,56 +8,112 @@ const Backdrop = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-end;
+  align-items: flex-end;
   background-image: linear-gradient(
-      to top,
+      to bottom,
       rgba(18, 18, 18, 1),
       rgba(18, 18, 18, 0) 95%
     ),
-    url("https://image.tmdb.org/t/p/original/kkm4UQdZveEqkDEubmWxa16z8lX.jpg");
+    url("https://i.redd.it/jlahbk0nv4f41.png");
   background-size: cover;
+  background-position: 40%;
+  position: relative;
+`;
+const Container = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  right: 5%;
+  bottom: 28%;
 `;
 const Original = styled.div`
-  font-size: 20px;
+  width: 460px;
+  font-size: 36px;
   font-weight: 500;
-  margin-bottom: 10px;
-  margin-left: 3px;
-  letter-spacing: 3px;
+  padding: 10px 5px 7px 5px;
+  margin-bottom: 35px;
+  letter-spacing: 4px;
   opacity: 0.8;
+  text-align: right;
 `;
 const Title = styled.div`
-  font-size: 50px;
-  font-weight: 700;
-  margin-bottom: 20px;
-  line-height: 65px;
-  opacity: 0.8;
+  font-size: 110px;
+  font-weight: 600;
+  letter-spacing: 2px;
+  margin-bottom: 35px;
+  text-align: right;
+  font-family: "Noto Serif KR", serif;
 `;
 const Span = styled.span`
-  color: #70a1ff;
+  font-size: 22px;
+  background: linear-gradient(
+    to right,
+    rgba(255, 255, 255, 1),
+    rgba(255, 255, 255, 0.3)
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
+const Bar = styled.div`
+  width: 460px;
+  height: 3px;
+  background: linear-gradient(
+    to right,
+    rgba(23, 192, 235, 1),
+    rgba(23, 192, 235, 0.1)
+  );
+  position: absolute;
+  top: 55px;
 `;
 const Summary = styled.div`
-  width: 340px;
-  font-size: 20px;
-  font-weight: 600;
-  letter-spacing: 10px;
-  margin-bottom: 10px;
-  padding: 0 0 10px 3px;
-  border-bottom: 2px solid #c2c2c2;
+  font-size: 25px;
+  font-weight: 500;
+`;
+const Button = styled.button`
+  margin-left: 20px;
+  margin-right: 5px;
+  &:nth-child(2) {
+    margin-right: 10px;
+  }
+  &:hover {
+    background-color: #f2f2f2;
+    color: #020202;
+  }
+  width: 100px;
+  height: 40px;
+  border: 3px solid #f2f2f2;
+  font-size: 18px;
+  font-weight: 500;
+  background: none;
+  color: #f2f2f2;
+  transition: all 0.3s ease-in-out;
+  cursor: pointer;
+  border-radius: 4px;
+`;
+const ButtonContainer = styled.div`
   display: flex;
-  justify-content: center;
 `;
 
 const MainPoster = () => {
   return (
     <Backdrop>
-      <Original>
-        いつかこの恋を思い出してきっと<Span>泣いて</Span>しまう
-      </Original>
-      <Title>
-        언젠가 이 사랑을 떠올리면
-        <br />
-        분명 <Span>울어버릴 것</Span> 같아.
-      </Title>
-      <Summary>사랑, 이별, 그리고 청춘.</Summary>
+      <Container>
+        <Original>
+          황혼 , <Span>"세상의 윤곽이 흐려지는 시간."</Span>
+        </Original>
+        <Bar />
+        <Title>너의 이름은.</Title>
+        <ButtonContainer>
+          <Button>
+            <i class="fas fa-play" style={{ "margin-right": "7px" }}></i>재생
+          </Button>
+          <Button>
+            <i class="fas fa-cart-plus" style={{ "margin-right": "5px" }}></i>
+            찜하기
+          </Button>
+        </ButtonContainer>
+      </Container>
     </Backdrop>
   );
 };
